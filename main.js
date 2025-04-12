07c572d69918b718fa9f6f8356cda5c14030d72c
EvanPear <esp.builds@gmail.com>
2025-04-12 09:03:00 -0700

    Add: Tie Breaker function and some alerts
    Added a tie breaker function to prevent ties and alerts to inform user

--- main.js
+++ main.js
@@ -8,16 +8,15 @@ function getComputerChoice() {
     if (randomNumber === 1 || randomNumber === 2) {
        return rock;
     } else if (randomNumber === 3 || randomNumber === 4){
-       return paper;
-    } else {
        return scissors;
+    } else {
+       return paper;
     }
    }
    
-   
-   
-   function getHumanChoice() {
-       let userInput = prompt ("What will it be? choose between: Rock, Paper, or Scissors. ").toLowerCase();
+    function getHumanChoice() {
+       let userInput = 
+        prompt ("What will it be? choose between: Rock, Paper, or Scissors. ").toLowerCase();
    
    
        while (userInput !== "rock" && userInput !== "paper" && userInput !== "scissors") {
@@ -52,7 +51,7 @@ function getComputerChoice() {
          console.log(`Round: ${roundCounter} Win! Score: You ${humanScore} - Computer ${computerScore}`);
          roundCounter++;
        } else if (humanChoice === computerChoice) {
-         console.log(`Player: ${[humanChoice]} - Computer: ${computerChoice}`)
+         console.log(`Player: ${humanChoice} - Computer: ${computerChoice}`)
          console.log(`Round: ${roundCounter} Draw! Score: You ${humanScore} - Computer ${computerScore}`);
          roundCounter++;
        } else {
@@ -70,13 +69,57 @@ function getComputerChoice() {
         playRound(humanSelection, computerSelection);
     }
 
+    
     if ( humanScore === computerScore) {
-        console.log(`Its a draw! ${humanScore} - ${computerScore}`);
+        console.log(`Tie Breaker Round Initiated Score: You ${humanScore} - Computer ${computerScore}`)
+        alert("Tie Breaker Round! Best of three - winner takes all!");
+        tieBreaker();
     } else if (humanScore > computerScore) {
         console.log(`Victory! ${humanScore} - ${computerScore}`);
     } else {
         console.log(`Defeat! ${computerScore} - ${humanScore}`);
     }
+
+    /* Who likes to end in a tie? tieBreaker() ensures they're always a winner */
+
+    function tieBreaker() {
+        let humanWins = 0;
+        let computerWins = 0;
+        let bonusRounds = 0;
+
+
+        while (humanWins < 2 && computerWins < 2) {
+            const humanChoice = getHumanChoice();
+            const computerChoice = getComputerChoice();
+        
+        if (
+            (humanChoice === rock && computerChoice === scissors) ||
+            (humanChoice === scissors && computerChoice === paper) ||
+            (humanChoice === paper && computerChoice === rock)
+        ) {
+            humanWins++;
+            bonusRounds++;
+            console.log(`You won tie breaker round: ${bonusRounds}!`);
+            console.log(`Score: You ${humanWins} - Computer ${computerWins}`);
+        } else if (humanChoice === computerChoice) {
+            console.log("Tie detected - round washed! Go again...");
+            tieBreaker();  
+        } else {
+            bonusRounds++;
+            computerWins++;
+            console.log(`You lost tie breaker round ${bonusRounds}!`);
+            console.log(`Score: You ${humanWins} - Computer ${computerWins}`);
+        }
+            
+    }
+    if (humanWins === 2) {
+        console.log("Victory!");
+        alert(`Victory, Congratulations! Final Score: You ${humanWins + humanScore} - Computer ${computerWins + computerScore}`);
+    } else if (computerWins === 2) {
+        console.log("Defeat!");
+        alert(`Defeat! Final Score: You ${humanWins + humanScore} - Computer ${computerWins + computerScore}`);
+    }
+  }
 }   
 
 playGame()
