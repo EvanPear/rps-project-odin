83e1d3b880b24a4167e3d1dcffd92ccd6cb2cb73
EvanPear <esp.builds@gmail.com>
2025-05-19 13:12:23 -0700

    style: fix function indentation and scope changes
    Improved code readability with corrected indentation.
    
    Moved maxRounds from inside the function to the global scope.
    
    removed tieBreaker() to prepare a full rewrite of the function.

--- main.js
+++ main.js
@@ -47,87 +47,52 @@ resetBtn.addEventListener('click', resetGame);
     let humanScore = 0;
     let computerScore = 0;
     let roundCounter = 0;
-
+    const maxRounds = 5;
+    
     function playRound(humanChoice, computerChoice) {
-
-        const maxRounds = 5;
 
         if (
             (humanChoice === rock && computerChoice === scissors) ||
             (humanChoice === paper && computerChoice === rock) || 
             (humanChoice === scissors && computerChoice === paper) 
          ) {
-         resultLog.textContent = (`Player: ${humanChoice} - Computer: ${computerChoice}`);
-         humanScore++;
-         roundCounter++;
-         roundInfo.textContent = (`Round: ${roundCounter} Win!`);
-         scoreBoard.textContent = (`You: ${humanScore} - Computer: ${computerScore}`);
+            resultLog.textContent = (`Player: ${humanChoice} - Computer: ${computerChoice}`);
+            humanScore++;
+            roundCounter++;
+            roundInfo.textContent = (`Round: ${roundCounter} Win!`);
+            scoreBoard.textContent = (`You: ${humanScore} - Computer: ${computerScore}`);
        } else if (humanChoice === computerChoice) {
-         resultLog.textContent = (`Player: ${humanChoice} - Computer: ${computerChoice}`);
-         roundCounter++;
-         roundInfo.textContent = (`Round: ${roundCounter} Draw!`);
-         scoreBoard.textContent = (`You: ${humanScore} - Computer: ${computerScore}`);
+            resultLog.textContent = (`Player: ${humanChoice} - Computer: ${computerChoice}`);
+            roundCounter++;
+            roundInfo.textContent = (`Round: ${roundCounter} Draw!`);
+            scoreBoard.textContent = (`You: ${humanScore} - Computer: ${computerScore}`);
        } else {
-         resultLog.textContent = (`Player: ${humanChoice} - Computer: ${computerChoice}`);
-         computerScore++;
-         roundCounter++;
-         roundInfo.textContent = (`Round: ${roundCounter} Loss!`);
-         scoreBoard.textContent = (`You: ${humanScore} - Computer: ${computerScore}`);
+            resultLog.textContent = (`Player: ${humanChoice} - Computer: ${computerChoice}`);
+            computerScore++;
+            roundCounter++;
+            roundInfo.textContent = (`Round: ${roundCounter} Loss!`);
+            scoreBoard.textContent = (`You: ${humanScore} - Computer: ${computerScore}`);
        }
+       
        if (maxRounds === roundCounter) {
          if (humanScore > computerScore) {
-         finalResult.textContent = (`Victory congratulations! Final score: You ${humanScore} - Computer ${computerScore}`);
+            finalResult.textContent = (`Victory congratulations! Final score: You ${humanScore} - Computer ${computerScore}`);
        } else {
-        finalResult.textContent = (`Defeat You lost this one! Final score: You ${humanScore} - Computer ${computerScore}`);
-      }
-      disableButtonChoices(true);
-      resetBtn.style.display = 'inline-block';
-     }
+            finalResult.textContent = (`Defeat You lost this one! Final score: You ${humanScore} - Computer ${computerScore}`);
+       }
+            disableButtonChoices(true);
+            resetBtn.style.display = 'inline-block';
     }
+}
 
 
    
 
     /* Who likes to end in a tie? tieBreaker() ensures they're always a winner */
 
-    function tieBreaker() {
-        let humanWins = 0;
-        let computerWins = 0;
-        let bonusRounds = 0;
-
-
-        while (humanWins < 2 && computerWins < 2) {
-            const humanChoice = getHumanChoice();
-            const computerChoice = getComputerChoice();
-        
-        if (
-            (humanChoice === rock && computerChoice === scissors) ||
-            (humanChoice === scissors && computerChoice === paper) ||
-            (humanChoice === paper && computerChoice === rock)
-        ) {
-            humanWins++;
-            bonusRounds++;
-            console.log(`You won tie breaker round: ${bonusRounds}!`);
-            console.log(`Score: You ${humanWins} - Computer ${computerWins}`);
-        } else if (humanChoice === computerChoice) {
-            console.log("Tie detected - round washed! Go again...");
-            continue; /*`continue` instead of calling `tieBreaker()`again stops score from resetting on a tie*/
-        } else {
-            bonusRounds++;
-            computerWins++;
-            console.log(`You lost tie breaker round ${bonusRounds}!`);
-            console.log(`Score: You ${humanWins} - Computer ${computerWins}`);
-        }
-            
-    }
-    if (humanWins === 2) {
-        console.log("Victory!");
-        alert(`Victory, Congratulations! Final Score: You ${humanWins + humanScore} - Computer ${computerWins + computerScore}`);
-    } else if (computerWins === 2) {
-        console.log("Defeat!");
-        alert(`Defeat! Final Score: You ${humanWins + humanScore} - Computer ${computerWins + computerScore}`);
-    }
-  }
+//     function tieBreaker() {
+    
+//   }
 
 
 rockBtn.addEventListener('click', () => {
